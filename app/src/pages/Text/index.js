d3e61a19aff45f9c7b56a7c1f6f3458311b95a1a
Carlos Daniel <carlos.daniel@devsquad.email>
2022-03-22 20:16:56 -0300

    wip: implement auto save each 500ms

--- app/src/pages/Text/index.js
+++ app/src/pages/Text/index.js
@@ -1,10 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect, useCallback } from 'react'
+import debounce from 'lodash/debounce'
 import { useLocation } from 'react-router-dom'
+import { load, save } from '../../services/api'
+
 import './styles.css'
 
 const Text = () => {
   const [text, setText] = useState('')
   const link = useLocation().pathname.replace('/', '')
+
+  useEffect(() => {
+    (async () => {
+      const response = await load(link)
+      setText(response.data || '')
+    })()
+  }, [link])
+
+  const debounced = useCallback(debounce(() => {
+    console.log('saving')
+    console.log({ text })
+    save(link, text)
+  }, 500), [text])
+
+  const handleSave = (e) => {
+    e.preventDefault()
+    console.log('before debounce', e.target.value)
+    setText(e.target.value)
+    debounced()
+  }
 
   return (
     <div className="container">
@@ -12,7 +35,11 @@ const Text = () => {
         <strong>Digite algo abaixo e o texto salva automaticamente! :)</strong>
       </div>
       <div className="textarea-container">
-        <textarea id="textarea"/>
+        <textarea
+          id="textarea"
+          value={text}
+          onChange={handleSave}
+        />
       </div>
     </div>
   )
