097cc744796134a541a9b3a5fc5ad84def117996
Carlos Daniel <carlos.daniel@devsquad.email>
2022-04-22 13:37:18 -0300

    chore: fix debug prefix

--- src/websocket.js
+++ src/websocket.js
@@ -1,14 +1,14 @@
 import debug from 'debug';
 import { startConnection, endConnection } from './services/websocket';
 
-export const connect = async ({ requestContext: { connectionId } }) => {
-  debug(`${process.env.STACK_NAME}:websocket:connect`)(`Connecting ${connectionId}`);
-  await startConnection(connectionId);
+export const connect = async ({ requestContext: { connectionId }, queryStringParameters: { link, connId } }) => {
+  debug(`${process.env.PROJECT_NAME}:websocket:connect`)('connectionId %o link %o connId %o', connectionId, link, connId);
+  await startConnection(connectionId, link, connId);
   return { statusCode: 200, body: 'Connected' };
 };
 
 export const disconnect = async ({ requestContext: { connectionId } }) => {
-  debug(`${process.env.STACK_NAME}:websocket:disconnect`)(`Disconnecting ${connectionId}`);
+  debug(`${process.env.PROJECT_NAME}:websocket:disconnect`)(`Disconnecting ${connectionId}`);
   await endConnection(connectionId);
   return { statusCode: 200, body: 'Disconnected' };
 };
